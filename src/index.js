3141ec507c7d604450117fbd02d576139ccc4263
Jess Nguyen <lovsand2001@gmail.com>
2017-11-23 18:15:13 -0800

    add onSearch change

--- src/index.js
+++ src/index.js
@@ -18,17 +18,22 @@ class App extends Component{
          selectedVideo: null
       };
 
-      YTSearch({key: API_KEY, term:'reactjs'}, (videos)=>{
-        this.setState({videos:videos,
-            selectedVideo:videos[0]
-        });
-     });
+      this.videoSearch('reactjs');
+      
+    }
+
+    videoSearch(term){
+        YTSearch({key: API_KEY, term:term}, (videos)=>{
+            this.setState({videos:videos,
+                selectedVideo:videos[0]
+            });
+         });
     }
     
     render(){
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={term=>this.videoSearch(term)} />
                 <VideoDetail  video={this.state.selectedVideo} />
                 <VideoList onVideoSelect={selectedVideo=>this.setState({selectedVideo})} 
                 videos={this.state.videos} />
